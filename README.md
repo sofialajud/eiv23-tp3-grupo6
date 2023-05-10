660b8e389dc229122d698ef3097e7b2a6b81f917
aclagoria <lagoriacarolina@gmail.com>
2023-05-10 17:54:16 -0300

    Fuerza bruta mas comentarios

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Descripción general del controlador de luz de escalera:
 
-    ** Funcionalidad básica: ** El controlador de luz de escalera es un dispositivo al cual se conectan un conjunto de pulsadores normal abiertos dispuestos en paralelo. Cuando algún pulsador cierra el circuito el controlador enciende la luz de escalera y la mantiene encendida durante un minuto. Finalizado el tiempo se apaga la luz.
+**Funcionalidad básica:** El controlador de luz de escalera es un dispositivo al cual se conectan un conjunto de pulsadores normal abiertos dispuestos en paralelo. Cuando algún pulsador cierra el circuito el controlador enciende la luz de escalera y la mantiene encendida durante un minuto. Finalizado el tiempo se apaga la luz.
 
-    ** Modo mudanza: ** Si un pulsador se presiona tres veces seguidas, la luz se enciende y permanece encendida hasta que vuelva a presionarse tres veces, en cuyo caso la luz se apaga y el equipo retorna a modo normal.
+**Modo mudanza:** Si un pulsador se presiona tres veces seguidas, la luz se enciende y permanece encendida hasta que vuelva a presionarse tres veces, en cuyo caso la luz se apaga y el equipo retorna a modo normal.
 
     .
     .
--- src/main.c
+++ src/main.c
@@ -1,28 +1,36 @@
 #include <stm32f1xx.h>
 
-int main (void)
-{
-//////ejemplos de expresiones and or xor etc
+#define LUZ_ON 0           
+#define LUZ_OFF 1           
+#define PULSADOR_ACTIVO 0  
+#define PULSADOR_NORMAL 1  
 
-    unsigned volatile A=0x800000000U, B=1U<<2, C=1U<<4, D=1U<<7;
+int main(void){ 
+        RCC->APB2ENR |= RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN; // Máscara para encender los puertos B y C 
+
+    // B9 pulsador, con resistencia pull hacia el nivel normal
+    GPIOB->BSRR = 1 << (9+((PULSADOR_NORMAL)? 0:16)); /*Evalúa la expresión que está a la izquierda de "?" si es verdadero el resultado de la expresión
+                                                      es lo que está a la izquierda de ":" si es falso lo que está a la derecha.
+                                                      BSRR registro que tiene dos partes de 16 bit, la primera es la parte de BIT SET, la segunda es la de BIT RESET
+                                                      Aquí si pulsador normal es verdadero o distinto de 0 escribe un 1 en la posición 9+0 osea 9 (lo pone en 1)
+                                                      si pulsador normal es 0 escribe un 1 en la posición 9+16=25, el 9 de la segunda parte (lo pone en 0)*/ 
+    GPIOB->CRH =   (GPIOB->CRH & ~(GPIO_CRH_MODE9 | GPIO_CRH_CNF9)) /* Lee el registro CRH (donde está la configuración del pin 9). 
+                                                                    Modifica en dos partes: 1) Puesta a cero: pone a 0 los bits de configuración del pin 9*/
+                    | GPIO_CRH_CNF9_1;                           //         2) BIT SET: pone a 1 solo los bits de configuración que se suponen que estén a 1
     
-    //bit set
-    A | = B;
-    A | = C;
-    A | = D;
-
-    //bit clear
-    A&= B;
-    A&= C;
-
-    //bit flip
-    A&= B;//not b - simbolo de la ondita 
-    A&= C;
-//////
-
-
-        RCC->APB2ENR | = RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN;
-        
-        GPIOC-> CRH = (CPIOC->CRH & //ONDITA etc  ) 
-
+    // C13 luz, inicialmente apagada
+    GPIOC->BSRR = 1 << (13+((LUZ_OFF)? 0:16));        // Si LUZ_OFF es: distinto de 0 escribe un 1 en la posición 13 (lo pone en 1), 0 escribe un 1 en la posición 13 de la segunda parte (lo pone en 0)
+    GPIOC->CRH =   (GPIOC->CRH & ~(GPIO_CRH_MODE13 | GPIO_CRH_CNF13)) // Lee, puesta a cero
+                    | GPIO_CRH_MODE13_1;                              // Set a 1
+                    
+    for(;;){ // Lazo principal infinito
+        while((GPIOB->IDR & (1<<9)) != (PULSADOR_ACTIVO << 9)); /* Lee el registro IDR (de entrada) y aplica una máscara  donde está en 1 el bit 9 se compara con
+                                                                PULSADOR_ACTIVO  desplazado 9 lugares*/ 
+        GPIOC->BSRR = 1 << (13+((LUZ_ON)? 0:16)); //Encender la luz
+        for(int j=0;j<60000;++j){ //Retardo por fuerza bruta
+            for(int volatile i=0;i<(8000000/(1000*13));++i); //Calibro para que sea de 1 milisegundo, Calificar como volatile para que el compilador no lo elimine
+        }
+        GPIOC->BSRR = 1 << (13+((LUZ_OFF)? 0:16)); //Apago la luz
+    }
+    return 0;
 }
